f2e6ea9c58cd5949c937a5b2dfdb198fb6af96b1
aadib <vikramaaditya10@gmail.com>
2022-12-07 17:12:41 +0530

    route and theme added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'home_page.dart';
+import 'package:flutter_projects/pages/login_page.dart';
+import 'pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,6 +13,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     int days = 30;
-    return MaterialApp(home: HomePage());
+    return MaterialApp(
+      // home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      initialRoute: "/login",
+      routes: {
+        "/": (context) => LoginPage(), // iska  / kamatlab hi home h
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage()
+      },
+      //Required property for darktheme
+    );
   }
 }
